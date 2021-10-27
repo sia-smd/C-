0b40f9ecdbeab41fc4b592705643a292d8ea9e6d
sia-smd <siavashsamadi@gmail.com>
2021-10-28 01:05:03 +0330

    final

--- project 1/Program.cs
+++ project 1/Program.cs
@@ -6,7 +6,7 @@ namespace project_1
     {
         static void Main(string[] args)
         {
-            int a=30;
+            int a = 30;
             string[,] std = new string[a, 2];
             string[,] mardod = new string[a, 2];
             int j = 0;
@@ -14,11 +14,12 @@ namespace project_1
             //  student name
             for (int i = 0; i < a; i++)
             {
-                System.Console.WriteLine("enter student name number" + (i + 1));
+                System.Console.WriteLine("enter student name #" + (i + 1));
                 std[i, 0] = Console.ReadLine();
                 count++;
                 if (std[i, 0] == "no")
                 {
+                    count--;
                     break;
                 }
 
@@ -30,27 +31,51 @@ namespace project_1
                 {
                     System.Console.WriteLine("enter mark of " + std[i, 0]);
                     std[i, 1] = Console.ReadLine();
-                      if (Convert.ToInt32(std[i, 1]) < 10)
-                      {
-                          mardod[j, 0] = std[i, 0];
-                          mardod[j, 1] = std[i, 1];
-                          j++;
-                      }
+                    if (Convert.ToInt32(std[i, 1]) < 10)
+                    {
+                        mardod[j, 0] = std[i, 0];
+                        mardod[j, 1] = std[i, 1];
+                        j++;
+                    }
                 }
                 else
                 {
                     break;
                 }
             }
+
+
             // mardodin
             System.Console.WriteLine("********************");
             System.Console.WriteLine("mardoodin");
             for (int i = 0; i < j; i++)
             {
-                System.Console.Write("student name:"+mardod[i,0]);
-                System.Console.WriteLine("|mark:"+mardod[i,1]);
+                System.Console.Write(mardod[i, 0]);
+                System.Console.WriteLine(" |mark:" + mardod[i, 1]);
             }
+
+
             // sort
+            int max = 0;
+            System.Console.WriteLine("************");
+            System.Console.WriteLine("ranking");
+
+            for (int i = 0; i < count; i++)
+            {
+                max = Convert.ToInt32(std[0, 1]);
+                int p = 0;
+                for (int m = 0; m < count; m++)
+                {
+                    if (max < Convert.ToInt32(std[m, 1]))
+                    {
+                        max = Convert.ToInt32(std[m, 1]);
+                        p = m;
+                    }
+                }
+                System.Console.Write($"#{i + 1}:" + std[p, 0]);
+                System.Console.WriteLine(" |mark: " + std[p, 1]);
+                std[p, 1] = "0";
+            }
         }
     }
 }
