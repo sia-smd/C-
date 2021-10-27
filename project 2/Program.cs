a0964386faf0ef0732d5ccb180a795943f9a4a17
sia-smd <siavashsamadi@gmail.com>
2021-10-27 22:46:05 +0330

    project 2

---
+++ project 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
