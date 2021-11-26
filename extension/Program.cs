0ad63cb301653b71ab83a9a1a196696d0d8eb7fa
sia-smd <siavashsamadi@gmail.com>
2021-11-26 13:53:41 +0330

    new

---
+++ extension/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace extension
+{
+    class Program
+    {
+        delegate void d(int a,int b);
+        static void Main(string[] args)
+        {
+            string s = DateTime.Now.toshamsi();
+            System.Console.WriteLine(s);
+            // متد با چند خروجي
+
+            int q, w;
+            int c = sum(5, 6, out q, out w);
+            System.Console.WriteLine(c);
+            System.Console.WriteLine(q);
+            System.Console.WriteLine(w);
+
+            List<int> w1 = new List<int> { 1, 2, 3, 6, 8, 12, 23 };
+            System.Console.WriteLine(w1.fard());
+            System.Console.WriteLine(w1.fardL());
+            //  deligate 
+            d x=new d(sum);
+            x+=zarb;
+            x+=div;
+            x(1,3);
+
+
+
+        }
+
+        public static void sum(int a, int b)
+        {
+            System.Console.WriteLine(a + b);
+        }
+        public static void div(int a, int b)
+        {
+            System.Console.WriteLine(a / b);
+        }
+        public static void zarb(int a, int b)
+        {
+            System.Console.WriteLine(a * b);
+        }
+
+
+        public static int sum(int a, int b, out int z, out int x)
+        {
+            z = ++a;
+            x = --b;
+            return a + b;
+        }
+    }
+    //  براي  extention  
+    // كلاس استاتيك
+    public static class shamsi
+    {
+        // متد استاتيك
+        // كلمه this  
+        //داشته باشه
+        public static string toshamsi(this DateTime a)
+        {
+            PersianCalendar s = new PersianCalendar();
+            // چه مدلي بنويسه
+            //   tostring("00")  دو رقمي بنويسه 
+            // tostring("#.0")    12,000,000
+            string date = s.GetYear(a) + "/" + s.GetMonth(a).ToString("00") + "/" + s.GetDayOfMonth(a).ToString("00") + "  " + s.GetHour(a).ToString("00") + ":" + s.GetMinute(a).ToString("00");
+
+            return date;
+        }
+        public static int fard(this List<int> a)
+        {
+            int sum = 0;
+            foreach (var item in a)
+            {
+                if (item % 2 != 0)
+                {
+                    sum = sum + item;
+                }
+
+            }
+            return sum;
+
+        }
+        public static int fardL(this List<int> a)
+        {
+
+            return a.Where(p => p % 2 != 0).Sum();
+
+        }
+
+
+
+    }
+}
--- linq/Program.cs
+++ linq/Program.cs
@@ -1,29 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace linq
 {
     class Program
     {
         static void Main(string[] args)
         {
+           employer e=new employer();
+           e.add();
+           e.sort();
+         
             
         }
     }
-    class student
+    class employer
     {
         int id;
-        string name;
-        int riazi;
-        int shimi;
-        public static void add()
+        string Name;
+        int Age;
+        string Gender;
+        int Price;
+        string Job;
+
+        public static int i=1;
+
+        public static List<employer> employer_list=new List<employer>();
+
+        public void add()
         {
-           List<student> st = new List<student>();
-           student s= new student();
-           s.id=1;
-           s.name="sia";
-           s.riazi=12;
-           s.shimi=15;
+            // do
+            // {
+              employer f= new employer();  
+            //   emp.id=i;
+            //   System.Console.Write($"enter name employer #{i}:");
+            //   emp.name=Console.ReadLine();
+            //   System.Console.Write($"enter age employer #{i}:");
+            //   emp.age=int.Parse(Console.ReadLine());
+            //   System.Console.Write($"enter gender employer #{i}:");
+            //   emp.gender=bool.Parse(Console.ReadLine());
+            //   System.Console.Write($"enter salary employer #{i}:");
+            //   emp.salary=int.Parse(Console.ReadLine());
+            //   System.Console.Write($"enter job locatiob employer #{i}:");
+            //   emp.job_location=Console.ReadLine();
+            //   employer_list.Add(emp);
+            //   i++;
+            // } while (i<5);
+
+                       
+            f.Name="ALi";
+            f.Price=16000;
+            f.Gender="Male";
+            f.Age=47;
+            f.Job="Admin";
+            employer_list.Add(f);
+            employer g= new employer();
+            g.Name="sara";
+            g.Price=2700;
+            g.Gender="Female";
+            g.Age=27;
+            g.Job="Operator";
+            employer_list.Add(g);
+             employer k= new employer();
+            k.Name="Mohammad";
+            k.Price=5500;
+            k.Gender="Male";
+            k.Age=23;
+            k.Job="G_Father";
+            employer_list.Add(k);
+            employer o= new employer();
+            o.Name="Mona";
+            o.Price=12000;
+            o.Gender="Female";
+            o.Age=32;
+            o.Job="Full_Developer";
+            employer_list.Add(o);
+             employer t= new employer();
+            t.Name="Hasan";
+            t.Price=9000;
+            t.Gender="Male";
+            t.Age=26;
+            t.Job="Front_Developer";
+            employer_list.Add(t);
+        }
+
+        public void sort()
+        {
+            var l1=employer_list.OrderByDescending(p => p.Age>40).ToList();
+            l1.ForEach (k => System.Console.WriteLine(k.Name+"-"+k.Age));
+
+        }
+         public void salry()
+        {
+            var l1=employer_list.OrderBy(p => p.Price>10000 && p.Price<13000 ).ToList();
+            l1.ForEach (k => System.Console.WriteLine(k.Name+"-"+k.Age));
+
+        }
+         public void female()
+        {
+            var l1=employer_list.Any(p => p.Gender=="Female" && p.Price<3000 );
+            System.Console.WriteLine("is female lower salary ? "+l1);
+
+        }
+        public void sum()
+        {
+            int sum=employer_list.Sum(p => p.Price);
+            System.Console.WriteLine("sum all salary :"+sum);
+
+            var avg=employer_list.Where(k => k.Gender=="Male").Average( a => a.Price);
+            System.Console.WriteLine("male salary avrage"+avg);
+            var maxP=employer_list.Max(l=>l.Price);
+            var max=employer_list.Where(f=>f.Price==maxP).FirstOrDefault();
+            System.Console.WriteLine("max location"+max.Job);
+            var avgAge=employer_list.Average(A=> A.Age);
+            System.Console.WriteLine("avrage all age"+avgAge);
+
+
         }
     }
 }
